4777639afc3569d48b6a9d9bfeaf26c4f0cd2c1e
Oksana <oksana.kyrychuk1991@gmail.com>
2019-07-20 15:55:08 +0300

    Changed mobile menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-// $('[data-mobile]').click(function() {
-//     var selector = $(this).attr('data-mobile');
+$('[data-mobile]').click(function() {
+    var selector = $(this).attr('data-mobile');
     
-//     if (selector === 'close') {
-//       $('.overlay').removeClass('active');
-//       $('.mobile-nav').removeClass('active');
-//     }
-//     else {
-//       $('.overlay').addClass('active');
-//       $('.mobile-nav').addClass('active');
-//     }
-//   });
+    if (selector === 'close') {
+      $('.overlay').removeClass('active');
+      $('.mobile-nav').removeClass('active');
+    }
+    else {
+      $('.overlay').addClass('active');
+      $('.mobile-nav').addClass('active');
+    }
+  });
